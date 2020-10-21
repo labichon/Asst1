237badb766a41718c463949f23f8f6723736f399
Tarek Z. Mohammed <tzm6@ilab1.cs.rutgers.edu>
2020-10-21 19:03:49 -0400

    started project, set up basics

---
+++ mymalloc.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+//#include "mymalloc.h"
+
+char memory[4096];
+
+//if free = 1, structure is free; if free = 0, structure is not free
+typedef struct block {
+	size_t size;
+	int free;
+	struct block *next;
+} Block;
+
+Block *List = (void*)memory;
+
+//initialize List
+void initialize (){
+
+	List->size = 4096 - sizeof(Block);
+	List->free = 1;
+	List->next = NULL;
+}
+
+
+//actual malloc function
+void *mymalloc (size_t numberOfBytes){
+
+	//pointers to track linked list
+	Block *curr;
+	Block *prev;
+	//pointer to track start of address of allocated memory
+	void *result;
+
+	//if List is not initialized, then it becomes initialized
+        if(!(List->size)){
+		initialize();
+	}
+	//set pointer
+	curr = List;
+
+	//if the metadata block cannot be used, we traverse until we find one that can be used
+	while((((curr->size)<numberOfBytes)||((curr->free) == 0)) && (curr->next != NULL)) {
+		prev = curr;
+		curr = curr->next;
+	}
+
+	//metadata block is exactly the size needed
+        if((curr->size)==numberOfBytes){
+		List->size = 0;
+		result = (void*)(++curr);
+		return result;
+	} //we need to make way for new block allocation if size needed by splitting new block
+}
+
+
+int main(){
+}
